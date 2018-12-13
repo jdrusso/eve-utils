907370fc914a42acd254cf791d1a55a958ad3077
JD Russo <jrusso@udel.edu>
2018-12-12 23:48:33 -0500

    Reworked to use graphviz and dicts

--- graph_analysis.py
+++ graph_analysis.py
@@ -12,56 +12,7 @@
 
 import requests_cache, redis
 from graph_tool.all import *
-from eve_utils import requests_retry_session
-
-def find_adjacent(system_id):
-
-
-    system_info = requests_retry_session().get(\
-        ('https://esi.evetech.net/latest/universe/'
-        'systems/{0}/'
-        '?datasource=tranquility&language=en-us').format(system_id),
-        timeout=15).json()
-
-    system_name = system_info['name']
-
-
-
-    try:
-        gates = system_info["stargates"]
-    except KeyError: # usually means this is a Jovian system
-        # print(system_info)
-        # print("ERROR")
-        return -1
-
-
-    out_systems = []
-
-    for gate in gates:
-
-        gate_info = requests_retry_session().get(
-            ('https://esi.evetech.net/latest/universe/'
-            'stargates/{0}/'
-            '?datasource=tranquility').format(gate),
-            timeout=15).json()
-
-        out_system = gate_info["destination"]["system_id"]
-
-        out_info = requests_retry_session().get(\
-            ('https://esi.evetech.net/latest/universe/'
-            'systems/{0}/'
-            '?datasource=tranquility&language=en-us').format(out_system),
-            timeout=15).json()
-
-
-        sec = out_info["security_status"]
-
-        out_systems += [out_system]
-
-    print(out_systems)
-
-    return system_name, out_systems
-
+from eve_utils import *
 
 
 if __name__=="__main__":
@@ -69,36 +20,54 @@ if __name__=="__main__":
 
     requests_cache.install_cache('graph_cache', backend="redis")
 
-    region_ids = ["10000048"]
+    region_ids = ["10000048"] # Placid
 
     systems = []
 
+    sdict = dict()
+
     for region_id in region_ids:
 
-        r_request = requests_retry_session().get(
-            ('https://esi.evetech.net/latest/'
-            'universe/regions/{region}/'
-            '?datasource=tranquility&language=en-us')
-            .format(region=region_id))
+        r_request = get_endpoint('/universe/regions/{0}'.format(region_id))
 
-        constellations = r_request.json()['constellations']
+        constellations = r_request['constellations']
 
         systems_list = []
 
         for constellation in constellations:
 
-            c_request = requests_retry_session().get(
-                ('https://esi.evetech.net/latest'
-                '/universe/constellations/{constellation}'
-                '/?datasource=tranquility&language=en-us')
-                .format(constellation=constellation))
+            c_request = get_endpoint('/universe/constellations/{0}'.format(constellation))
 
-            systems_list += c_request.json()['systems']
+            systems_list += c_request['systems']
 
         # Make a list of all systems in the region
         systems = [[system] for system in systems_list]
 
+        for system in systems_list:
+            sdict[system] = {}
+
         print("Obtained %d systems" % len(systems))
+
+        kill_data = get_endpoint('/universe/system_kills')
+        for kill in kill_data:
+            if kill['system_id'] in systems_list:
+                sdict[kill['system_id']]['npc_kills'] = int(kill['npc_kills'])
+                sdict[kill['system_id']]['ship_kills'] = int(kill['ship_kills'])
+
+        jump_data = get_endpoint('/universe/system_jumps')
+        for jump in jump_data:
+            if jump['system_id'] in systems_list:
+                sdict[jump['system_id']]['ship_jumps'] = int(jump['ship_jumps'])
+
+
+        for system in sdict.keys():
+            if 'npc_kills' not in sdict[system].keys():
+                sdict[system]['npc_kills'] = 0
+                sdict[system]['ship_kills'] = 0
+            if 'ship_jumps' not in sdict[system].keys():
+                sdict[system]['ship_jumps'] = 0
+
+        print("Gathered system kill and jump data")
 
 
         # Build a list of [systems, [adjacent systems]]
@@ -106,35 +75,52 @@ if __name__=="__main__":
 
             print("Parsing system %d / %d" % (i+1, len(systems)), end='\r')
 
-            name, adjacent = find_adjacent(systems[i][0])
-            # print(adjacent)
-            systems[i].append(name)
-            systems[i].append(adjacent)
+            id = systems[i][0]
 
-        print([system for system in systems])
+            adjacent = get_adjacent(id)
+            name = get_name(id)
 
-        print("\n\n Done parsing systems...")
+            sec = get_endpoint('/universe/systems/{0}'.format(id))['security_status']
+
+            sdict[id]['adjacent'] = adjacent
+            sdict[id]['name'] = name
+            sdict[id]['sec'] = float(sec)
+
+
+        # print([system for system in systems])
+
+        print("\nDone parsing systems...")
 
     # At this point, I have a list of all the systems in a region.
     # Go through them, find connections
 
-    # TODO: Do this earlier
-    # Build the dict of systems
-    sdict = dict()
-    for system in systems:
-        sdict[system[0]] = {'adjacent': system[2], 'name': system[1]}
 
     # Build the graph
     print("Generating graph.")
 
-    graph = Graph()
+    graph = Graph(directed=False)
 
     # Add all the vertices
     # vertices = [graph.add_vertex() for system in systems]
     vprop_names = graph.new_vertex_property("string")
+    vprop_sec = graph.new_vertex_property("float")
+    vprop_kills = graph.new_vertex_property("int")
+    vprop_jumps = graph.new_vertex_property("int")
     for system in sdict.keys():
+
+        # If system is highsec:
+        if sdict[system]['sec'] >= 0.45:
+            continue
+
         sdict[system]['vertex'] = graph.add_vertex()
-        vprop_systemname[sdict[system]['vertex']] = sdict[system]['name']
+        vprop_names[sdict[system]['vertex']] = sdict[system]['name']
+        vprop_sec[sdict[system]['vertex']] = sdict[system]['sec']
+        vprop_kills[sdict[system]['vertex']] = sdict[system]['ship_kills']
+        vprop_jumps[sdict[system]['vertex']] = sdict[system]['ship_jumps']
+
+
+    vprop_size = prop_to_size(vprop_kills, mi=0, ma=5)
+
 
 
     # Add edges
@@ -146,8 +132,24 @@ if __name__=="__main__":
                 pass
             # adjacent is the system ID of the adjacent
 
-    print(graph.list_properties())
-
     # Draw graph
-    graph_draw(graph, vertex_text=vprop_names, vertex_font_size=18,
-        output_size=(2048,2048), output="two-nodes.png")
+    # graph_draw(graph,
+    #     vertex_text=vprop_names,
+    #     vertex_size=vprop_size,
+    #     vertex_font_size=10,
+    #     output_size=(1200,768),
+    #     edge_pen_width=24,
+    #     output="graph.png")
+    graphviz_draw(graph,
+        # vertex_text=vprop_names,
+        vsize=vprop_size,
+        size=(100,100),
+        overlap="prism",
+        ratio='fill',
+        # vertex_font_size=10,
+        # output_size=(1200,768),
+        # edge_pen_width=24,
+        vprops={'label':vprop_names},
+        vcolor=prop_to_size(vprop_jumps, ma=1),
+        # sep=1,
+        output="graph.png")
