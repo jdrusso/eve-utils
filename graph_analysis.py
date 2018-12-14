e1cb67801415e26b669d6cb65423545a49025a7c
JD Russo <jrusso@udel.edu>
2018-12-14 15:38:31 -0500

    Added comments <3

--- graph_analysis.py
+++ graph_analysis.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
+
 # Just some stuff playing with a graph theory analysis of systems and
 #   connections between them.
+# The main idea is to represent the star systems in a region/cluster of regions
+#   as a graph.
+# Vertex weights can be set by whatever relevant parameter -- since my goal is
+#   to locate a path that provides "optimal pvp", my relevant metrics are ship
+#   kills and jumps.
+# It's also conceivable that NPC kills would be a useful statistic, depending
+#   on the kind of hunting you're doing, so I store that data as well.
 
-
-# Probably need to generate a list of systems with their sec status and connections
-#   or something to start with.
-
-
-# Pull all the systems in Placid
-# For each system, get everything it's connected to
-# Build a dict of "system": "adjacent"
 
 import requests_cache, redis, sys
 from graph_tool.all import *
@@ -57,18 +58,26 @@ if __name__=="__main__":
 
     osti_id = 30003792
 
+    # This can be disabled, but be warned that it will take many many moons
+    #   to gather all your data every time you run this.
+    # The backend can be switched to sqlite3, BUT the sqlite3 cache doesn't
+    #   handle multiple concurrent requests particularly well.
+    # I stick with redis so that if I ever want to poll huge amounts of systems,
+    #   the option for multithreading is still open.
     requests_cache.install_cache('graph_cache', backend="redis")
 
-    region_ids = ["10000048", "10000051", "10000041", "10000023", "10000069", "10000064", "10000068"] # Placid
+    # Yeah, yeah, magic numbers. Whatever.
+    # First ID here is placid, the rest are all the adjacent regions.
+    # region_ids = ["10000048", "10000051", "10000041", "10000023", "10000069", "10000064", "10000068"] # Placid
+    region_ids = ["10000048"]#, "10000051", "10000041", "10000023", "10000069", "10000064", "10000068"] # Placid
+
 
     systems = []
-
     sdict = dict()
 
     for region_id in region_ids:
 
         r_request = get_endpoint('/universe/regions/{0}'.format(region_id))
-
         constellations = r_request['constellations']
 
         systems_list = []
@@ -76,29 +85,32 @@ if __name__=="__main__":
         for constellation in constellations:
 
             c_request = get_endpoint('/universe/constellations/{0}'.format(constellation))
-
             systems_list += c_request['systems']
 
         # Make a list of all systems in the region
         systems = [[system] for system in systems_list]
 
-        for system in systems_list:
-            sdict[system] = {}
+        # This dict is used later
+        sdict = {system:{} for system in systems_list}
 
         print("Obtained %d systems" % len(systems))
 
+        # Pull all kill data for the region
         kill_data = get_endpoint('/universe/system_kills')
         for kill in kill_data:
             if kill['system_id'] in systems_list:
                 sdict[kill['system_id']]['npc_kills'] = int(kill['npc_kills'])
                 sdict[kill['system_id']]['ship_kills'] = int(kill['ship_kills'])
 
+        # Pull all jump data for the region
         jump_data = get_endpoint('/universe/system_jumps')
         for jump in jump_data:
             if jump['system_id'] in systems_list:
                 sdict[jump['system_id']]['ship_jumps'] = int(jump['ship_jumps'])
 
-
+        # The ESI API doesn't return anything if the system had 0 kills/jumps,
+        #   so look for anything that hasn't had its values set and manually
+        #   set it to 0.
         for system in sdict.keys():
             if 'npc_kills' not in sdict[system].keys():
                 sdict[system]['npc_kills'] = 0
@@ -125,19 +137,17 @@ if __name__=="__main__":
             sdict[id]['name'] = name
             sdict[id]['sec'] = float(sec)
 
-
-        # print([system for system in systems])
-
         print("\nDone parsing systems...")
 
     # At this point, I have a list of all the systems in a region.
     # Go through them, find connections
 
-    # Remove highsec
+    # Remove highsec systems
     sdict = {x:sdict[x] for x in sdict.keys() if sdict[x]['sec'] < 0.45}
 
 
     # Build the graph
+    # All stargates are two-way, so it's undirected.
     print("Generating graph.")
     graph = Graph(directed=False)
 
@@ -156,30 +166,28 @@ if __name__=="__main__":
         vprop_jumps[sdict[system]['vertex']] = sdict[system]['ship_jumps']
 
 
+    # Rescale the number of kills per system to a function that lives in [0,5],
+    #   which is a more appropriate range for vertex sizes when drawing.
     vprop_size = prop_to_size(vprop_kills, mi=0, ma=5)
 
 
 
-    # Add edges
+    # Add edges connecting vertices
+    # These edges represent stargates between systems.
     for system in sdict.keys():
-        # if sdict[system]['name'] == "Covryn":
-        #
-        #     print("\nanalyzing system %s" % sdict[system]['name'])
-        #     print("Adjacent are %s" % sdict[system]['adjacent'])
 
         source = sdict[system]['vertex']
 
         for adjacent in sdict[system]['adjacent']:
-            # if debug:
-            #     print(sdict[adjacent][])
             try:
                 target = sdict[adjacent]['vertex']
             except KeyError:
-                continue # Not in the selected regions
+                # The adjacent system found is not in the selected regions,
+                #   so ignore it.
+                continue
 
-            # if sdict[system]['name'] == "Covryn":
-            #     print("making edge to %s" % vprop_names[target])
-
+            # Only add the edge if it hasn't already been added.
+            # Without this, I'd get doubles of everything
             if (graph.edge(source, target) == None) and (graph.edge(target,source) == None):
                 graph.add_edge(source, target)
 
@@ -213,6 +221,7 @@ if __name__=="__main__":
     max_kills = 0
     for route in routes:
 
+        # Get statistics for the route
         route_names = [vprop_names[s] for s in route]
         route_kills = sum([vprop_kills[s] for s in route])
         route_jumps = sum([vprop_jumps[s] for s in route])
@@ -229,24 +238,14 @@ if __name__=="__main__":
         max_kills,
         sum([vprop_jumps[s] for s in best_route])))
 
-    # vprop_onroute = graph.new_vertex_property("bool")
-    # for v in graph.vertices():
-    #     if v in best_route:
-    #         vprop_onroute[v] = True
-    #     else:
-    #         vprop_onroute[v] = False
 
+    # Mark which edges are along the route
     eprop_onroute = graph.new_edge_property("bool")
-    # Detect which edges are along the route
     for e in graph.edges():
         if e.source() in best_route and e.target() in best_route:
             eprop_onroute[e] = 6
         else:
             eprop_onroute[e] = 1
-
-    # Draw the route
-    # graph.set_edge_filter(eprop_onroute)
-
 
 
     # Draw graph
